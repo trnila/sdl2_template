da3ac43ca8cf4a80df8056aa02168312a14df3d2
Daniel Trnka <daniel.trnka@gmail.com>
2020-11-21 19:40:18 +0100

    Whitespaces fix

--- sdl.c
+++ sdl.c
@@ -3,71 +3,71 @@
 #include "sdl.h"
 
 SDL_Context* sdl_context_new(const char* window_name, int width, int height) {
-    SDL_Context *ctx = malloc(sizeof(SDL_Context));
-    if(ctx == NULL) {
-      fprintf(stderr, "Could not allocate memory.");
+  SDL_Context *ctx = malloc(sizeof(SDL_Context));
+  if(ctx == NULL) {
+    fprintf(stderr, "Could not allocate memory.");
+    exit(1);
+  }
+
+  // Init SDL with video and audio
+  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
+    fprintf(stderr, "SDL_Init Error: %s\n", SDL_GetError());
+    exit(1);
+  }
+
+  // Init TrueType fonts
+  if(TTF_Init() != 0) {
+    fprintf(stderr, "TTF_Init Error: %s\n", TTF_GetError());
+    exit(1);
+  }
+
+  // Init audio with support for .ogg, .opus and .mp3
+  int flags = MIX_INIT_OGG | MIX_INIT_OPUS | MIX_INIT_MP3;
+  if (flags != (Mix_Init(flags))) {
+    fprintf(stderr, "Mix_Init Error: %s\n", Mix_GetError());
+    exit(1);
+  }
+
+  if(Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, AUDIO_S16SYS, 2, 640) != 0) {
+    fprintf(stderr, "Mix_OpenAudio Error: %s\n", Mix_GetError());
+    exit(1);
+  }
+
+  bool enableFullscreen = width <= 0 || height <= 0;
+  ctx->window = SDL_CreateWindow(
+    window_name,
+    SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+    width, height,
+    SDL_WINDOW_SHOWN | (enableFullscreen ? SDL_WINDOW_FULLSCREEN_DESKTOP : 0)
+  );
+  if (ctx->window == NULL) {
+    fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
+    exit(1);
+  }
+
+  // create GPU accelerated renderer and use vertical sync
+  ctx->renderer = SDL_CreateRenderer(ctx->window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+  if (ctx->renderer == NULL) {
+    fprintf(stderr, "SDL_CreateRenderer Error: %s\n", SDL_GetError());
+    exit(1);
+  }
+
+  // enable transparency
+  if(SDL_SetRenderDrawBlendMode(ctx->renderer, SDL_BLENDMODE_BLEND) != 0) {
+    fprintf(stderr, "SDL_SetRenderDrawBlendMode Error: %s\n", SDL_GetError());
+    exit(1);
+  }
+
+  if(enableFullscreen) {
+    if(SDL_GetRendererOutputSize(ctx->renderer, &width, &height) != 0) {
+      fprintf(stderr, "SDL_GetRendererOutputSize Error: %s\n", SDL_GetError());
       exit(1);
     }
+  }
 
-    // Init SDL with video and audio
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
-      fprintf(stderr, "SDL_Init Error: %s\n", SDL_GetError());
-      exit(1);
-    }
-
-    // Init TrueType fonts
-    if(TTF_Init() != 0) {
-      fprintf(stderr, "TTF_Init Error: %s\n", TTF_GetError());
-      exit(1);
-    }
-
-    // Init audio with support for .ogg, .opus and .mp3
-    int flags = MIX_INIT_OGG | MIX_INIT_OPUS | MIX_INIT_MP3;
-    if (flags != (Mix_Init(flags))) {
-      fprintf(stderr, "Mix_Init Error: %s\n", Mix_GetError());
-      exit(1);
-    }
-
-    if(Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, AUDIO_S16SYS, 2, 640) != 0) {
-      fprintf(stderr, "Mix_OpenAudio Error: %s\n", Mix_GetError());
-      exit(1);
-    }
-
-    bool enableFullscreen = width <= 0 || height <= 0;
-    ctx->window = SDL_CreateWindow(
-      window_name,
-      SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-      width, height,
-      SDL_WINDOW_SHOWN | (enableFullscreen ? SDL_WINDOW_FULLSCREEN_DESKTOP : 0)
-    );
-    if (ctx->window == NULL) {
-      fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
-      exit(1);
-    }
-
-    // create GPU accelerated renderer and use vertical sync
-    ctx->renderer = SDL_CreateRenderer(ctx->window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    if (ctx->renderer == NULL) {
-      fprintf(stderr, "SDL_CreateRenderer Error: %s\n", SDL_GetError());
-      exit(1);
-    }
-
-    // enable transparency
-    if(SDL_SetRenderDrawBlendMode(ctx->renderer, SDL_BLENDMODE_BLEND) != 0) {
-      fprintf(stderr, "SDL_SetRenderDrawBlendMode Error: %s\n", SDL_GetError());
-      exit(1);
-    }
-
-    if(enableFullscreen) {
-      if(SDL_GetRendererOutputSize(ctx->renderer, &width, &height) != 0) {
-        fprintf(stderr, "SDL_GetRendererOutputSize Error: %s\n", SDL_GetError());
-        exit(1);
-      }
-    }
-
-    ctx->width = width;
-    ctx->height = height;
-    return ctx;
+  ctx->width = width;
+  ctx->height = height;
+  return ctx;
 }
 
 void sdl_context_delete(SDL_Context *ctx) {
